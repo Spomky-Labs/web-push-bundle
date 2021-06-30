ae5bf32bc57dc451517a85bb5064f2299d5f9a79
Florent Morselli <contact@spomky-labs.com>
2021-06-30 15:59:02 +0200

    WIP v1.1 (#4)
    * Use of param() function
    * Fix mysql reserved word "keys"
    * Fix tests
    * exipred subscription function renamed
    * Tests fixed
    * New interfaces and Symfony HttClient support for async requests
    * CS & PHPStan
    * dep fixed
    * Inapropriate method removed
    * Fix classname
    * Rector actions
    * New tests
    * Mutant blocked
    * Reduced MSI
    * Reduced Covered MSI

---
+++ Service/StatusReport.php
@@ -0,0 +1,92 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2020-2021 Spomky-Labs
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+namespace WebPush\Bundle\Service;
+
+use Symfony\Contracts\HttpClient\ResponseInterface;
+use WebPush\NotificationInterface;
+use WebPush\StatusReportInterface;
+use WebPush\SubscriptionInterface;
+
+class StatusReport implements StatusReportInterface
+{
+    private SubscriptionInterface $subscription;
+    private NotificationInterface $notification;
+    private ResponseInterface $response;
+    private ?int $code = null;
+    private ?string $location = null;
+    private ?array $links = null;
+
+    public function __construct(SubscriptionInterface $subscription, NotificationInterface $notification, ResponseInterface $response)
+    {
+        $this->subscription = $subscription;
+        $this->notification = $notification;
+        $this->response = $response;
+    }
+
+    public function getSubscription(): SubscriptionInterface
+    {
+        return $this->subscription;
+    }
+
+    public function getNotification(): NotificationInterface
+    {
+        return $this->notification;
+    }
+
+    public function isSuccess(): bool
+    {
+        $code = $this->prepareStatusCode();
+
+        return $code >= 200 && $code < 300;
+    }
+
+    public function isSubscriptionExpired(): bool
+    {
+        $code = $this->prepareStatusCode();
+
+        return 404 === $code || 410 === $code;
+    }
+
+    public function getLocation(): string
+    {
+        if (null === $this->location) {
+            $headers = $this->response->getHeaders();
+            $this->location = implode(', ', $headers['location'] ?? ['']);
+        }
+
+        return $this->location;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getLinks(): array
+    {
+        if (null === $this->links) {
+            $headers = $this->response->getHeaders();
+            $this->links = $headers['link'] ?? [];
+        }
+
+        return $this->links;
+    }
+
+    private function prepareStatusCode(): int
+    {
+        if (null === $this->code) {
+            $this->code = $this->response->getStatusCode();
+        }
+
+        return $this->code;
+    }
+}
---
+++ Service/WebPush.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2020-2021 Spomky-Labs
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+namespace WebPush\Bundle\Service;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use WebPush\ExtensionManager;
+use WebPush\Loggable;
+use WebPush\NotificationInterface;
+use WebPush\SubscriptionInterface;
+use WebPush\WebPushService;
+
+class WebPush implements WebPushService, Loggable
+{
+    private HttpClientInterface $client;
+    private LoggerInterface $logger;
+    private RequestFactoryInterface $requestFactory;
+    private ExtensionManager $extensionManager;
+
+    public function __construct(HttpClientInterface $client, RequestFactoryInterface $requestFactory, ExtensionManager $extensionManager)
+    {
+        $this->client = $client;
+        $this->requestFactory = $requestFactory;
+        $this->extensionManager = $extensionManager;
+        $this->logger = new NullLogger();
+    }
+
+    public function setLogger(LoggerInterface $logger): self
+    {
+        $this->logger = $logger;
+
+        return $this;
+    }
+
+    public function send(NotificationInterface $notification, SubscriptionInterface $subscription): StatusReport
+    {
+        $this->logger->debug('Sending notification', ['notification' => $notification, 'subscription' => $subscription]);
+        $request = $this->requestFactory->createRequest('POST', $subscription->getEndpoint());
+        $request = $this->extensionManager->process($request, $notification, $subscription);
+        $this->logger->debug('Request ready', ['request' => $request]);
+
+        $response = $this->client->request('POST', $subscription->getEndpoint(), [
+            'body' => $request->getBody()->getContents(),
+            'headers' => $request->getHeaders(),
+        ]);
+        $this->logger->debug('Response received', ['response' => $response]);
+
+        return new StatusReport(
+            $subscription,
+            $notification,
+            $response
+        );
+    }
+}
